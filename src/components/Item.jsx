80f86362aadc89589cdba39129646dc78319e7ba
Alejo <alejohurtadotesta@gmail.com>
2023-09-17 14:32:11 -0300

    Modificamos las imagenes y le saacamos el width por defecto del componente Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,7 +4,7 @@ export default function Item({ id, title, picture }) {
   return (
     <div className="producto">
       <h1 className="name-produc">{title}</h1>
-      <img src={picture} alt={title} width={300} />
+      <img src={picture} alt={title} />
       <Link to={`/item/${id}`} className="detail-produc">
         Ver detalles del producto
       </Link>
